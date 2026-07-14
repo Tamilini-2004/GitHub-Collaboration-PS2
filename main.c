dabf56fa12e4b6bd7fc401e0fcd5d55076b5d848
Nithya <mail4nithyashree@gmail.com>
2026-07-14 06:07:44 +0530

    Added multiplication and division operations

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main()
 {
+    int a = 20, b = 10;
+
     printf("GitHub Collaboration Exercise\n");
+
+    printf("Multiplication = %d\n", a * b);
+    printf("Division = %d\n", a / b);
+
     return 0;
 }
